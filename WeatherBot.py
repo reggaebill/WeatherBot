60a0b6fc4e1677b598d26a2c62f8b30517961d12
aaron <aarongerhart7@gmail.com>
2025-02-23 22:16:14 -0500

    API Json parsed, geocode service init, hourly forecast functions defined working

--- WeatherBot.py
+++ WeatherBot.py
@@ -1,11 +1,74 @@
+import json
+
 import discord
+
+import requests
+from geopy import Nominatim
+from geopy.geocoders import nominatim
+
+weather_api = open('C:\\Users\\aaron\\Downloads\\openapi.json')
+json.load(weather_api)
 
 from discord.ext import commands
 
 #TODO declare bot functions here
+def parse_day(day):
+    time_list = day.split("T")
+    time_day = time_list[0].split('-')
+    time_day = time_day[1:]
+    day_final = f'{time_day[0]}-{time_day[1]}'
+    return day_final
+
+def parse_time(time):
+    time_list = time.split('T')
+    time_time = time_list[1].split('-')
+    hour_time = time_time[0].split(':')
+    hour_time = hour_time[:2]
+    hour_time = f'{hour_time[0]}:{hour_time[1]}'
+    return hour_time
+def get_geocode(location):     #turns text location into coordinates
+    location = geolocator.geocode(location)
+    print(location.latitude, location.longitude)
+    return (f'{location.latitude},{location.longitude}')
+
+def callWeather(x):      #sends lat, long to API, parses the returned info, and formats text
+    hourly_outlook = ''
+    i = 0
+    response = requests.get(f'https://api.weather.gov/points/{x}')
+    if response.status_code == 200:
+        data = response.json()
+        #forecast = data.get('forecastHourly')
+        hourly_data = requests.get(data['properties']['forecastHourly'])
+        hourly_data_parsed = hourly_data.json()
+        list_data = hourly_data_parsed['properties']['periods']
+        for item in list_data:
+            day = parse_day(item['startTime'])
+            time = parse_time(item['startTime'])
+            temp = f'{item['temperature']}:{item['temperatureUnit']}'
+            outlook = f'{item['shortForecast']}'
+            hourly_outlook += f'{day} : {time} | {temp} | {outlook}\n'
+            i += 1
+            if i == 25:
+                break
+        return hourly_outlook
+    else:
+        return "Wouldn't you like to know, weather boy"
+
+
+
+
+
+#intialize geopy
+geolocator = Nominatim(user_agent="weatherbot")
+#initialize requests module
+r = requests.get('https://api.weather.gov')
+r.status_code
+
+r.headers['content-type'] = 'application/json'
+r.encoding = 'utf-8'
 
 #token: MTM0MzM1NzIzMDMwNDU5MjAwNQ.GjVA4D.FtCugN4KGMi0JCFpPq-HQLSuHuag00DQCQRn0w
-
+#initialize discord api
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
@@ -21,6 +84,11 @@ async def on_message(message):
 @client.event
 async def on_ready():
     message.send('dis the bot')
+
+@bot.command()
+async def hourly(message, postal_code):
+    coordinates = get_geocode(postal_code)
+    await message.send(callWeather(coordinates))
 
 @bot.command()
 async def ping(message):
