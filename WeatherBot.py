634e78257c241bcacba1f992df0a840b44593914
aaron <aarongerhart7@gmail.com>
2025-02-23 22:23:08 -0500

    cleanup and comments

--- WeatherBot.py
+++ WeatherBot.py
@@ -1,25 +1,22 @@
 import json
-
 import discord
-
 import requests
 from geopy import Nominatim
-from geopy.geocoders import nominatim
 
+#load weather API
 weather_api = open('C:\\Users\\aaron\\Downloads\\openapi.json')
 json.load(weather_api)
 
 from discord.ext import commands
 
-#TODO declare bot functions here
-def parse_day(day):
+def parse_day(day):        #splits up the time parameter and returns day only
     time_list = day.split("T")
     time_day = time_list[0].split('-')
     time_day = time_day[1:]
     day_final = f'{time_day[0]}-{time_day[1]}'
     return day_final
 
-def parse_time(time):
+def parse_time(time):     #splits up the time parameter and returns hour only
     time_list = time.split('T')
     time_time = time_list[1].split('-')
     hour_time = time_time[0].split(':')
@@ -37,7 +34,6 @@ def callWeather(x):      #sends lat, long to API, parses the returned info, and 
     response = requests.get(f'https://api.weather.gov/points/{x}')
     if response.status_code == 200:
         data = response.json()
-        #forecast = data.get('forecastHourly')
         hourly_data = requests.get(data['properties']['forecastHourly'])
         hourly_data_parsed = hourly_data.json()
         list_data = hourly_data_parsed['properties']['periods']
@@ -52,14 +48,16 @@ def callWeather(x):      #sends lat, long to API, parses the returned info, and 
                 break
         return hourly_outlook
     else:
-        return "Wouldn't you like to know, weather boy"
+        return "https://tenor.com/view/wouldnt-you-like-to-know-weather-boy-gif-15559185"
 
 
 
 
 
 #intialize geopy
+
 geolocator = Nominatim(user_agent="weatherbot")
+
 #initialize requests module
 r = requests.get('https://api.weather.gov')
 r.status_code
@@ -67,14 +65,14 @@ r.status_code
 r.headers['content-type'] = 'application/json'
 r.encoding = 'utf-8'
 
-#token: MTM0MzM1NzIzMDMwNDU5MjAwNQ.GjVA4D.FtCugN4KGMi0JCFpPq-HQLSuHuag00DQCQRn0w
 #initialize discord api
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 client = discord.Client(intents=intents)
 
-
+#discord bot commands
 
 @client.event
 async def on_message(message):
@@ -94,5 +92,5 @@ async def hourly(message, postal_code):
 async def ping(message):
     await message.send('pong')
 
-    #message.send('https://tenor.com/view/wouldnt-you-like-to-know-weather-boy-gif-15559185')
+
 bot.run('MTM0MzM1NzIzMDMwNDU5MjAwNQ.GjVA4D.FtCugN4KGMi0JCFpPq-HQLSuHuag00DQCQRn0w')
